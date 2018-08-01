875462ff4f70511b0a149bf3f4a1787d86489a6c
huhu <bianlongting@dianwoba.com>
2018-08-01 15:47:53 +0800

    optimizr

--- lib/index.tsx
+++ lib/index.tsx
@@ -33,8 +33,8 @@ commander
 commander.on('--help', function(){
     console.log('\n Examples:');
     console.log('');
-    console.log('  $ snake -h');
-    console.log('  $ snake init snake-demo ');
+    console.log('  $ parrot -h');
+    console.log('  $ parrot init parrot-demo ');
     console.log('');
   });
 
@@ -47,7 +47,7 @@ help()
 const release = async() => {
   const nodeVersion = execSync("node -v", { encoding: "utf8" });
   if (process.argv.length === 2) {
-    execSync('snake -h')
+    execSync('parrot -h')
   }
   if (!compareVersion(nodeVersion)) {
     console.log('Please make sure the node version is above 8.0'.red);
