952fd8a381550ba6a12464866d1b5752b82a4a97
huhu <bianlongting@dianwoba.com>
2018-08-16 10:54:56 +0800

    add file-loader and redux-antd template

--- src/redux/webpack.production.js
+++ src/redux/webpack.production.js
@@ -1,16 +1,21 @@
 const path = require("path");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ManifestPlugin = require('webpack-manifest-plugin');
-const { publicPath, filename, template, favicon } = require("./package.json").config
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const {
+  publicPath,
+  filename,
+  template,
+  favicon
+} = require("./package.json").config;
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.[hash].js",
-    publicPath,
+    publicPath
   },
   resolve: {
     extensions: [".js", ".jsx", ".js", ".json"]
@@ -19,43 +24,57 @@ module.exports = {
     rules: [
       {
         test: /\.(js|jsx)/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.less$/,
-        include: path.resolve(__dirname, './node_modules'),
-        loader: 'style-loader!css-loader!less-loader?javascriptEnabled=true'
+        include: path.resolve(__dirname, "./node_modules"),
+        loader: "style-loader!css-loader!less-loader?javascriptEnabled=true"
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {}
+          }
+        ]
       },
       {
         test: /\.less$/,
-        exclude: path.resolve(__dirname, './node_modules'),
-        use: [{
+        exclude: path.resolve(__dirname, "./node_modules"),
+        use: [
+          {
             loader: "style-loader" // creates style nodes from JS strings
-        }, {
+          },
+          {
             loader: "css-loader", // translates CSS into CommonJS
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: "[name]__[local]___[hash:base64:5]",
+              localIdentName: "[name]__[local]___[hash:base64:5]"
             }
-        },{
+          },
+          {
             loader: "less-loader", // compiles Less to CSS
             options: {
               javascriptEnabled: true
             }
-        }, {
-          loader: 'postcss-loader'
-        }]
+          },
+          {
+            loader: "postcss-loader"
+          }
+        ]
       }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(["dist"]),
     new ManifestPlugin(),
     new HtmlWebpackPlugin({
       filename,
       template,
-      favicon,
+      favicon
     })
   ]
-}
+};
