be51ee3c335a5f75453936d84e67236274c1ca83
huhu <bianlongting@dianwoba.com>
2018-10-23 19:12:01 +0800

    upgrade babel7

--- src/express-react/webpack/common.js
+++ src/express-react/webpack/common.js
@@ -1,69 +1,95 @@
 const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, "../src/client/index.js"),
+  entry: path.resolve(__dirname, "../src/client/index"),
   output: {
     path: path.resolve(__dirname, "../dist"),
     filename: "bundle.js",
-    publicPath: "dist/"
+    publicPath: "dist/",
   },
   devtool: "source-map",
   resolve: {
     extensions: [".js", ".jsx", ".js", ".json"],
-    modules: [path.resolve(__dirname, "node_modules")]
+    modules: [path.resolve(__dirname, "../node_modules")],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)/,
-        loader: "babel-loader"
+        loader: "babel-loader",
       },
       {
         test: /\.less$/,
         include: path.resolve(__dirname, "../node_modules"),
-        loader: "style-loader!css-loader!less-loader?javascriptEnabled=true"
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          {
+            loader: "less-loader",
+            options: {
+              modifyVars: {
+                "primary-color": "#1DA57A",
+                "link-color": "#1DA57A",
+                "border-radius-base": "2px",
+              },
+              javascriptEnabled: true,
+            },
+          }, // 在这里可以定义antd主题
+          /**
+           {loader: 'less-loader', // compiles Less to CSS
+             options: {
+               modifyVars: {
+                 'primary-color': '#1DA57A',
+                 'link-color': '#1DA57A',
+                 'border-radius-base': '2px',
+               },
+               javascriptEnabled: true,
+              }
+            }
+           */
+        ],
       },
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
           {
             loader: "file-loader",
-            options: {}
-          }
-        ]
+            options: {},
+          },
+        ],
       },
       {
         test: /\.less$/,
         exclude: path.resolve(__dirname, "../node_modules"),
         use: [
           {
-            loader: "style-loader" // creates style nodes from JS strings
+            loader: "style-loader", // creates style nodes from JS strings
           },
           {
             loader: "css-loader", // translates CSS into CommonJS
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: "[name]__[local]___[hash:base64:5]"
-            }
+              localIdentName: "[name]__[local]___[hash:base64:5]",
+            },
           },
           {
             loader: "less-loader", // compiles Less to CSS
             options: {
-              javascriptEnabled: true
-            }
+              javascriptEnabled: true,
+            },
           },
           {
             loader: "postcss-loader",
             options: {
               // 如果没有options这个选项将会报错 No PostCSS Config found
               plugins: () => [
-                require("autoprefixer")() // CSS浏览器兼容
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  }
+                require("autoprefixer")(), // CSS浏览器兼容
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
